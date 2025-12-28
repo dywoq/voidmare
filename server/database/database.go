3e3242c2dab0db0c9dc249db1964a68eb0a3240f
dywoq <aleks.koyf@gmail.com>
2025-12-28 19:27:21 +0300

    server/database: Add Setup function
    Instead of making multiple functions (such as EnvSetup, ClientSetup
    etc.), I created Setup - which handles all these cases. It's generally
    recommended to use in main() function.

--- server/database/database.go
+++ server/database/database.go
@@ -6,24 +6,37 @@ type Database struct {
 	env *env.Management
 }
 
-// EnvSetup sets the environment variable requirements needed by the database,
-// and performs checking.
-// 
-// Returns an error if checking fails.
-func (d *Database) EnvSetup() error {
-	d.env.SetRequirement("DATABASE_URL")
-	d.env.SetRequirement("DATABASE_API_KEY")
-	return d.env.Check()
+func New() *Database {
+	return &Database{
+		env: &env.Management{},
+	}
+}
+
+// Setup runs all steps that are needed to make database function normally.
+// Recommended to run before using it.
+func (d *Database) Setup() error {
+	if err := d.envSetup(); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Url returns the database URL.
-// Use it after running EnvSetup.
+// Use it after running Setup.
 func (d *Database) Url() string {
 	return d.env.Get("DATABASE_URL")
 }
 
 // ApiKey returns the database API key.
-// Use it after running EnvSetup.
+// Use it after running Setup.
 func (d *Database) ApiKey() string {
 	return d.env.Get("DATABASE_API_KEY")
+}
+
+// related to setup
+
+func (d *Database) envSetup() error {
+	d.env.SetRequirement("DATABASE_URL")
+	d.env.SetRequirement("DATABASE_API_KEY")
+	return d.env.Check()
 }
