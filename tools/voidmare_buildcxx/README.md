5101bda51eb5a4ddd5b71184c7a1056eca912507
dywoq <dywoq.contact@gmail.com>
2026-09-12 22:35:45 +0300

    Delete tools/voidmare_buildcxx
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

--- tools/voidmare_buildcxx/README.md
+++
@@ -1,2 +0,0 @@
-The development tool `voidmare_buildcxx` is used within C/C++ projects of VOIDMARE
-to compile them and run their tests. It replaces CMake.
--- tools/voidmare_buildcxx/src/main.cxx
+++
@@ -1,7 +0,0 @@
-//
-// Copyright 2026 dywoq - All Rights Reserved
-// https://github.com/dywoq/voidmare
-//
-
-#include "voidmare/tools/buildcxx/master.h"
-
