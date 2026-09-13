29bd2d8cd6b75f94e99f274634794aad290d0cc8
dywoq <dywoq.contact@gmail.com>
2026-09-13 13:20:24 +0300

    base/std/integer.hxx: Draft of the IntegerBase class functionality
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/std/include/voidmare/std/integer.hxx
@@ -0,0 +1,169 @@
+//
+// Copyright 2026 dywoq - All Rights Reserved
+// https://github.com/dywoq/voidmare
+//
+// Module Description
+//
+// 		Functionality related to integers
+//
+
+#ifndef _VOIDMARE_STD_INTEGER_HXX
+#define _VOIDMARE_STD_INTEGER_HXX
+
+namespace Voidmare::Std::Integer
+{
+    //
+    // Routine Description
+    //
+    // 		This class is an unified way to manage integers at compile time,
+    // 		providing a rich set of functionality.
+    //
+    template <typename IntegralT> class IntegerBase
+    {
+        //
+        // TODO: When the standard library will have support of type traits,
+        // add a compile time assertion that checks if IntegralT is integral type.
+        //
+      private:
+        IntegralT m_Integer;
+
+      public:
+        constexpr explicit IntegerBase(IntegralT ProvidedInteger) noexcept : m_Integer(ProvidedInteger)
+        {
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Increases the underlying integer.
+        //
+        constexpr void Increase() noexcept
+        {
+            m_Integer++;
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Decreases the underlying integer.
+        //
+        constexpr void Decrease() noexcept
+        {
+            m_Integer--;
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Returns the underlying integer.
+        //
+        [[nodiscard]] constexpr IntegralT Value() const noexcept
+        {
+            return m_Integer;
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Adds the provided value to the underlying integer.
+        //
+        // Return
+        //
+        // 		Calculation result.
+        //
+        constexpr IntegralT Add(IntegralT Value) noexcept
+        {
+            IntegralT Result = m_Integer + Value;
+            m_Integer = Result;
+            return Result;
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Takes the provided value from the underlying integer.
+        //
+        // Return
+        //
+        // 		Calculation result.
+        //
+        constexpr IntegralT Minus(IntegralT Value) noexcept
+        {
+            IntegralT Result = m_Integer - Value;
+            m_Integer = Result;
+            return Result;
+        }
+
+        //
+        // Routine Description
+        //
+        // 		Divides the underlying integer by the provided value.
+        // 		Compilation process fails if the provided value is 0.
+        //
+        // Return
+        //
+        // 		Calculation result.
+        //
+        constexpr IntegralT Divide(IntegralT Value) noexcept
+        {
+            static_assert(Value != 0, "division by zero is forbidden");
+            IntegralT Result = m_Integer / Value;
+            m_Integer = Result;
+            return Result;
+        }
+
+        // Routine Description
+        //
+        // 		Multiplies the underlying integer by the provided value.
+        //
+        // Return
+        //
+        // 		Calculation result.
+        //
+        constexpr IntegralT Multiply(IntegralT Value) noexcept
+        {
+            IntegralT Result = m_Integer * Value;
+            m_Integer = Result;
+            return Result;
+        }
+    };
+
+    namespace Literals
+    {
+        constexpr IntegerBase<signed char> operator""_Int8(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<signed char>(Value));
+        }
+        constexpr IntegerBase<signed short> operator""_Int16(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<signed short>(Value));
+        }
+        constexpr IntegerBase<signed int> operator""_Int32(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<signed int>(Value));
+        }
+        constexpr IntegerBase<signed long long> operator""_Int64(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<signed long long>(Value));
+        }
+
+        constexpr IntegerBase<unsigned char> operator""_UInt8(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<unsigned char>(Value));
+        }
+        constexpr IntegerBase<unsigned short> operator""_UInt16(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<unsigned short>(Value));
+        }
+        constexpr IntegerBase<unsigned int> operator""_UInt32(unsigned long long Value)
+        {
+            return IntegerBase(static_cast<unsigned int>(Value));
+        }
+        constexpr IntegerBase<unsigned long long> operator""_UInt64(unsigned long long Value)
+        {
+            return IntegerBase(Value);
+        }
+    } // namespace Literals
+} // namespace Voidmare::Std::Integer
+
+#endif
--- base/std/src/Windows/x86_64/start.cxx
+++ base/std/src/Windows/x86_64/start.cxx
@@ -7,11 +7,29 @@
 // 		Starting routine of Windows x86_64
 //
 
+#include <voidmare/std/integer.hxx>
 #include <windows.h>
 
 extern "C" void VoidmareStd___WindowsStart()
 {
+    using namespace Voidmare;
+    using namespace Std::Integer;
+
+    //
+    // Initialization routine
+    //
+
+    //
+    // Call the main function.
+    //
+
     extern void Main();
     Main();
-    ExitProcess(1);
+
+    //
+    // Exit the process
+    //
+
+    auto ExitCode = IntegerBase<UINT>(0);
+    ExitProcess(ExitCode.Value());
 }
