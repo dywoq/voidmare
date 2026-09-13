54eda59c36012c6e61021c945c6bd3aa34c915fe
dywoq <dywoq.contact@gmail.com>
2026-09-13 09:36:59 +0300

    base/std: Initialize
    Signed-off-by: dywoq <dywoq.contact@gmail.com>

---
+++ base/std/src/Windows/x86_64/startup.cxx
@@ -0,0 +1,12 @@
+//
+// Copyright 2026 dywoq - All Rights Reserved
+// https://github.com/dywoq/voidmare
+//
+// Module Description
+//
+// 		Starting routine of Windows x86_64
+//
+
+extern "C" void mainCRTStartup()
+{
+}
